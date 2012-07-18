c0140ca9def0081161599fcdf17e172623806a8d
Alan Reyes <kg.designer@gmail.com>
2012-07-17 19:51:08 -0500

    Se eliminan los alias de las funciones de console para prevenir errores en Chrome

--- openmultimedia/debug/debug.js
+++ openmultimedia/debug/debug.js
@@ -2,18 +2,15 @@ goog.provide('openmultimedia.debug');
 
 
 /** @constructor */
-openmultimedia.debug.Console = function() {};
 
-openmultimedia.debug.Console.prototype.log = ( goog.DEBUG && console && console.log ) || goog.nullFunction;
+if ( ! (console && console.group) ) {
+  console.group = function() { console.info(arguments); }
+}
 
-openmultimedia.debug.Console.prototype.info = ( goog.DEBUG && console && console.info ) || goog.emptyFunction;
+if ( ! (console && console.groupColapsed) ) {
+  console.groupCollapsed = function() { console.info(arguments); }
+}
 
-openmultimedia.debug.Console.prototype.group = ( goog.DEBUG && console && ( console.group || console.info ) ) || goog.emptyFunction;
-
-openmultimedia.debug.Console.prototype.groupCollapsed = openmultimedia.debug.Console.prototype.group;
-
-openmultimedia.debug.Console.prototype.groupEnd = ( goog.DEBUG && console && ( console.groupEnd || console.info ) ) || goog.emptyFunction;
-
-openmultimedia.debug.nativeConsole_ = goog.global.console;
-
-goog.global.console = new openmultimedia.debug.Console();
+if ( ! (console && console.groupEnd) ) {
+  console.groupEnd = function() { console.info(arguments); }
+}
