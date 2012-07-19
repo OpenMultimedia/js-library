8e8ecb9387905bc35ecdc1916574cf51fe64df5d
Alan Reyes <kg.designer@gmail.com>
2012-07-19 16:23:42 -0500

    Agregada la opcion para pasar parametros a las templates HAML a traves de la variable de entorno HAML_PARAMS

--- build/rake/HamlBuilder.rb
+++ build/rake/HamlBuilder.rb
@@ -2,11 +2,20 @@ module OpenMultimedia
   module HamlBuilder
     def haml_build(args)
       source = args[:source] || raise(ArgumentError, "source parameter required")
-      target= args[:target] || raise(ArgumentError, "target parameter required")
+      target = args[:target] || raise(ArgumentError, "target parameter required")
+      params = args[:params]
 
       command = [ "haml" ]
 
       command << "--require" << File.join( File.dirname(__FILE__), "..", "haml/SoyUtils" )
+
+      if ( params and not params.empty? )
+        ENV["HAML_PARAMS"] = (params.collect do |name, value|
+          "#{name}=#{value}"
+        end).join(",")
+      else
+        ENV.delete "HAML_PARAMS"
+      end
 
       command << source << target
 
