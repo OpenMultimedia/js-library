cb3e56457d77595bfa894ba29133f6794e31fc55
Alan Reyes <kg.designer@gmail.com>
2012-07-23 00:40:47 -0500

    Correccion para evaluar parametros recibidos de linea de comandos en hojas de estilo SASS

--- build-library/sass/Functions.rb
+++ build-library/sass/Functions.rb
@@ -1,14 +1,16 @@
 module OpenMultimedia
   class Helper
-    def params
+    def param(name)
       if not defined? @params
         @params = {}
-        ENV["SASS_PARAMS"].split(",").collect do |value|
-          pieces = value.split("=")
-          @params[pieces[0]] = ((pieces.length > 1) && pieces[1]) || true
+        if ENV["SASS_PARAMS"]
+            ENV["SASS_PARAMS"].split(",").collect do |value|
+              pieces = value.split("=")
+              @params[pieces[0]] = ((pieces.length > 1) && pieces[1]) || "true"
+            end
         end
       end
-      @params
+      @params[name]
     end
   end
 end
@@ -16,7 +18,18 @@ end
 $OpenMultimediaHelper = OpenMultimedia::Helper.new
 
 module Sass::Script::Functions
-  def self.debug
-    Sass::Script::Bool.new($OpenMultimediaHelper.params["debug"] == "true")
-  end
+    def param(name)
+        val = $OpenMultimediaHelper.param(name.to_s)
+
+        case val
+        when "true" then
+            Sass::Script::Bool.new(true)
+        when "false", NilClass then
+            Sass::Script::Bool.new(false)
+        else
+            Sass::Script::String.new(val)
+        end
+    end
+
+    declare :param, [:name]
 end
